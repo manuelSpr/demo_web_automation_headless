5afd1762e3acb88a43ee60b15dff41528b68ddbc
manuel.silva <manuel.silva@irhythmtech.com>
2022-07-05 17:27:56 -0500

    update configuration

--- src/main/java/Utils/DriverConfig.java
+++ src/main/java/Utils/DriverConfig.java
@@ -12,15 +12,15 @@ public class DriverConfig {
 
   public static WebDriver setDriver(){
     driver = null;
-    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.setBinary("/usr/bin/chromedriver");
     options.addArguments("--headless", "--window-size=1920,1200");
-    //options.addArguments("--no-sandbox");
+    options.addArguments("--no-sandbox");
     //options.setHeadless(true);
     //options.addArguments("--disable-gpu");
     //options.addArguments("--disable-extensions");
-    //options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-dev-shm-usage");
     WebDriverManager.chromedriver().setup();
     //driver = WebDriver.Chrome("/usr/lib/chromium");
     driver = new ChromeDriver(options);
