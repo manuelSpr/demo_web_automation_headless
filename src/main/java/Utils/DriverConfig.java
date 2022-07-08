f0d37241580d13c782f7785256981b2cfe744032
manuel.silva <manuel.silva@irhythmtech.com>
2022-07-08 12:35:31 -0500

    update configuration

--- src/main/java/Utils/DriverConfig.java
+++ src/main/java/Utils/DriverConfig.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,38 +19,40 @@ public class DriverConfig {
 
   public static void setDriver() throws InterruptedException {
     System.out.println("hola");
-    ChromeDriverService service = new ChromeDriverService.Builder()
+    /*ChromeDriverService service = new ChromeDriverService.Builder()
         .usingPort(64185)
-        .build();
+        .build();*/
     //WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker();
     //driver = new WebDriverManager.chromedriver().browserInDocker();
     //driver = wdm.create();
     //System.out.println("hola a todos");
     //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
     //System.setProperty("webdriver.chrome.whitelistedIps", "");
+    ChromeDriverManager.getInstance().setup();
     ChromeOptions options = new ChromeOptions();
     options.setHeadless(true);
     options.addArguments("--no-sandbox");
     //options.addArguments("--headless");
-    options.addArguments("--verbose");
+    //options.addArguments("--verbose");
     //options.addArguments("--disable-gpu");
-    options.addArguments("--ignore-certificate-errors");
+    /*options.addArguments("--ignore-certificate-errors");
     options.addArguments("--allow-running-insecure-content");
     options.addArguments("--allow-insecure-localhost");
-    options.addArguments("--disable-web-security");
+    options.addArguments("--disable-web-security");*/
     //options.setBinary("/usr/bin/google-chrome");
     //options.addArguments("--headless", "--window-size=1920,1200");
-    options.addArguments("start-maximized"); // open Browser in maximized mode
+    //options.addArguments("start-maximized"); // open Browser in maximized mode
     //options.addArguments("disable-infobars"); // disabling infobars
     //options.setHeadless(true);
     //options.addArguments("--disable-extensions");
-    options.addArguments("--disable-dev-shm-usage");
-    WebDriverManager.chromedriver().browserInDocker();
+    //options.addArguments("--disable-dev-shm-usage");
+    //WebDriverManager.chromedriver().browserInDocker();
     //driver = WebDriver.Chrome("/usr/lib/chromium");
     //options.setBinary("/usr/bin/chromedriver");
-    driver = new ChromeDriver(service, options);
+    //driver = new ChromeDriver(service, options);
     //driver.get("https://www.google.com/");
     //wdm.getWebDriver().get("https://bonigarcia.dev/selenium-webdriver-java/");
+    driver = new ChromeDriver(options);
     Thread.sleep(5000);
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
     Thread.sleep(5000);
